e39fa9668ea56eab14e65733b540d6a9069bf4f0
tianyi <tianyi.dong@gmail.com>
2019-09-05 17:40:35 +0800

    change consul_service

--- sparrow_cloud/registry/service_discovery.py
+++ sparrow_cloud/registry/service_discovery.py
@@ -14,32 +14,36 @@ def get_settings_value(name):
         raise NotImplementedError("没有配置这个参数%s" % name)
     return value
 
-def _build_env_host_name(NAME):
-    ENV_SERVICE_HOST_KEY = NAME + "_HOST"
-    env_service_host = os.environ.get(ENV_SERVICE_HOST_KEY, "")
-    return env_service_host
+# def _build_env_host_name(name):
+#     ENV_SERVICE_HOST_KEY = name + "_HOST"
+#     env_service_host = os.environ.get(ENV_SERVICE_HOST_KEY, "")
+#     return env_service_host
 
-def consul_service(SERVICE_SETTINGS_KEY_NAME):
+def consul_service(service_conf):
     """
-    从consul service 拿到服务的名称
-    SERVICE_SETTINGS_KEY_NAME: settings 里面的服务注册 key 值:
-        例如:
-            SPARROW_PERMISSION_REGISTER_NAME = "sparrow-permission-svc"
-            服务调用方式:
-                consul_service("SPARROW_PERMISSION_REGISTER_NAME")
+    从consul service 拿到服务配置
+    "SERVICE_CONF": {
+        "ENV_NAME": "PERMISSION_REGISTER_NAME_HOST",
+        "VALUE": "sprrow-permission-svc"
+    }
+    ENV_NAME: 用来覆盖 consul 的环境变量名
+    VALUE: consul服务注册名字
     """
     # 获取 _HOST 对应的环境变量是否存在
-    # import pdb; pdb.set_trace()
-    env_service_host = _build_env_host_name(SERVICE_SETTINGS_KEY_NAME)
+
+
+    # service_conf= get_settings_value(service_conf)
+    name = service_conf['ENV_NAME']
+    env_service_host = os.environ.get(name, "")
     if env_service_host:
-        # 1 获取对应的环境变量, 如果环境变量存在, 返回对应的环境变量
+        # 获取对应的环境变量, 如果环境变量存在, 返回对应的环境变量
         return env_service_host
     else:
         # 对应的环境变量不存在, 使用服务发现
         consul_conf = get_settings_value('CONSUL_CLIENT_ADDR')
         consul_host = consul_conf.get('HOST', None)
         consul_port = consul_conf.get('PORT', None)
-        service_register_name = get_settings_value(SERVICE_SETTINGS_KEY_NAME)
+        service_register_name = service_conf['VALUE']
         if service_register_name:
             if (consul_host and consul_port) is None:
                 raise NotImplementedError("CONSUL_CLIENT_ADDR:consul_host={},consul_port={},必须同时配置")
