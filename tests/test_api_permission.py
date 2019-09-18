fa0efd35fc594d50da066331c2e60e1dfc35c23d
littletiger7 <little_tiger7@163.com>
2019-09-18 15:34:26 +0800

    add permission test

--- tests/test_api_permission.py
+++ tests/test_api_permission.py
@@ -2,12 +2,27 @@ import unittest
 import os
 from django.conf import settings
 from unittest import mock
+from django.http import JsonResponse
 
 
 class MockRequests(object):
     @property
     def path(self):
         return '/api/xx/xxx/xxxx/'
+
+    @property
+    def method(self):
+        return 'GET'
+
+    @property
+    def META(self):
+        return {'REMOTE_USER': '3213321131312313'}
+
+
+class MockRequests1(object):
+    @property
+    def path(self):
+        return '/api/xx/xxx/500/'
 
     @property
     def method(self):
@@ -32,6 +47,8 @@ def mocked_requests_get(*args, **kwargs):
 
     if args[0] == 'http://127.0.0.1:8500/api/xx/xxx/xxxx/':
         return MockResponse({"has_perm": True}, 200)
+    if args[0] == 'http://127.0.0.1:8500/api/xx/xxx/500/':
+        return MockResponse({"has_perm": True}, 500)
     return MockResponse({"has_perm": False}, 404)
 
 
@@ -40,6 +57,10 @@ class TestPermissionMiddleware(unittest.TestCase):
     def setUp(self):
         os.environ["DJANGO_SETTINGS_MODULE"] = "tests.mock_settings"
         os.environ["SPARROW_TEST_HOST"] = "127.0.0.1:8500"
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    @mock.patch('sparrow_cloud.utils.validation_data.VerificationConfiguration.valid_permission_svc', return_value='')
+    def test_has_permission(self, verify_middleware_location, mocked_requests_get):
         settings.PERMISSION_MIDDLEWARE = {
             # 权限验证服务的配置
             "PERMISSION_SERVICE": {
@@ -52,11 +73,64 @@ class TestPermissionMiddleware(unittest.TestCase):
             "SKIP_PERMISSION": False,
             # 是否启用权限中间件， SKIP_PERMISSION:True, 则跳过中间件， 如果不配置SKIP_PERMISSION，或者SKIP_PERMISSION:False，则不跳过中间件
         }
+        from sparrow_cloud.middleware.api_permission import PermissionMiddleware
+        self.assertEqual(PermissionMiddleware().process_request(MockRequests()), None)
 
     @mock.patch('requests.get', side_effect=mocked_requests_get)
     @mock.patch('sparrow_cloud.utils.validation_data.VerificationConfiguration.valid_permission_svc', return_value='')
-    def test_has_permission(self, verify_middleware_location, mocked_requests_get):
+    def test_permission_exception_400(self, verify_middleware_location, mocked_requests_get):
+        settings.PERMISSION_MIDDLEWARE = {
+            # 权限验证服务的配置
+            "PERMISSION_SERVICE": {
+                "ENV_NAME": "SPARROW_TEST_HOST",
+                # VALUE 为服务发现的注册名称
+                "VALUE": "sparrow",
+            },
+            "API_PATH": "/api/xx/xxx/ssss/",
+            "FILTER_PATH": [''],  # 使用权限验证中间件， 如有不需要验证的URL， 可添加到列表中
+            "SKIP_PERMISSION": False,
+            # 是否启用权限中间件， SKIP_PERMISSION:True, 则跳过中间件， 如果不配置SKIP_PERMISSION，或者SKIP_PERMISSION:False，则不跳过中间件
+        }
         from sparrow_cloud.middleware.api_permission import PermissionMiddleware
+        self.assertEqual(PermissionMiddleware().process_request(MockRequests()).status_code,
+                         JsonResponse({'msg': '400'}, status=400).status_code)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    @mock.patch('sparrow_cloud.utils.validation_data.VerificationConfiguration.valid_permission_svc', return_value='')
+    def test_permission_exception_500(self, verify_middleware_location, mocked_requests_get):
+        settings.PERMISSION_MIDDLEWARE = {
+            # 权限验证服务的配置
+            "PERMISSION_SERVICE": {
+                "ENV_NAME": "SPARROW_TEST_HOST",
+                # VALUE 为服务发现的注册名称
+                "VALUE": "sparrow",
+            },
+            "API_PATH": "/api/xx/xxx/500/",
+            "FILTER_PATH": [''],  # 使用权限验证中间件， 如有不需要验证的URL， 可添加到列表中
+            "SKIP_PERMISSION": False,
+            # 是否启用权限中间件， SKIP_PERMISSION:True, 则跳过中间件， 如果不配置SKIP_PERMISSION，或者SKIP_PERMISSION:False，则不跳过中间件
+        }
+        from sparrow_cloud.middleware.api_permission import PermissionMiddleware
+        PermissionMiddleware().process_request(MockRequests())
+        self.assertEqual(PermissionMiddleware().process_request(MockRequests1()), None)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    @mock.patch('sparrow_cloud.utils.validation_data.VerificationConfiguration.valid_permission_svc', return_value='')
+    def test_permission_skip_permission_and_filter_path(self, verify_middleware_location, mocked_requests_get):
+        settings.PERMISSION_MIDDLEWARE = {
+            # 权限验证服务的配置
+            "PERMISSION_SERVICE": {
+                "ENV_NAME": "SPARROW_TEST_HOST",
+                # VALUE 为服务发现的注册名称
+                "VALUE": "sparrow",
+            },
+            "API_PATH": "/api/xx/xxx/xxxx/",
+            "FILTER_PATH": ['/api/xx/xxx/xxxx/'],  # 使用权限验证中间件， 如有不需要验证的URL， 可添加到列表中
+            "SKIP_PERMISSION": 'False',
+            # 是否启用权限中间件， SKIP_PERMISSION:True, 则跳过中间件， 如果不配置SKIP_PERMISSION，或者SKIP_PERMISSION:False，则不跳过中间件
+        }
+        from sparrow_cloud.middleware.api_permission import PermissionMiddleware
+        PermissionMiddleware().process_request(MockRequests())
         self.assertEqual(PermissionMiddleware().process_request(MockRequests()), None)
 
     def tearDown(self):
