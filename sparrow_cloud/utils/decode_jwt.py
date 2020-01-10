7c1bb0dc413444fe23c39ef0521d95c4a167b182
littletiger7 <little_tiger7@163.com>
2020-01-10 18:11:33 +0800

    change decode_jwt

--- sparrow_cloud/utils/decode_jwt.py
+++ sparrow_cloud/utils/decode_jwt.py
@@ -15,21 +15,3 @@ def decode_jwt(token):
         raise ex
     return payload
 
-#
-#
-#
-# class DecodeJwt(object):
-#     """decode_jwt"""
-#
-#     def __init__(self, ):
-#         self.settings_value = GetSettingsValue()
-#         self.secret = self.settings_value.get_middleware_value(
-#             'JWT_MIDDLEWARE', 'JWT_SECRET')
-#
-#     def decode_jwt(self, token):
-#         secret = self.secret
-#         try:
-#             payload = jwt.decode(token, secret, algorithms='HS256')
-#         except Exception as ex:
-#             raise ex
-#         return payload
