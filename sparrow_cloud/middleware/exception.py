484c6dc1697191daf6856a9b9efc4010e4d8774e
littletiger7 <little_tiger7@163.com>
2020-03-22 18:17:05 +0800

    add

---
+++ sparrow_cloud/middleware/exception.py
@@ -0,0 +1,33 @@
+import logging
+import traceback
+from django.conf import settings
+from sparrow_cloud.dingtalk.sender import send_message
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except:
+    MiddlewareMixin = object
+
+logger = logging.getLogger(__name__)
+
+
+MESSAGE_LINE = """
+##### <font color=\"info\"> 服务名称: {service_name}</font> #####
+> 进程异常错误:<font color=\"warning\">{exception_info}</font>
+"""
+
+
+class ExceptionMiddleware(MiddlewareMixin):
+    def process_exception(self, request, exception):
+        debug = settings.DEBUG
+        code = getattr(settings, "CLOUD_ERROR_NOTIFICATION_ROBOT", "cloud_error_notification_robot")
+        service_name = getattr(settings, "SERVICE_CONF", None).get("NAME", None)
+        if debug is True:
+            pass
+        else:
+            exception_info = traceback.format_exc()[-800:-1]
+            try:
+                msg = MESSAGE_LINE.format(service_name=service_name, exception_info=exception_info)
+                logger.info("sparrow_cloud log, service process_exception info : {}".format(msg))
+                send_message(msg=msg, code_list=[code], channel="wechat", message_type="markdown")
+            except Exception as ex:
+                logger.error("sparrow_cloud 发送服务错误信息失败，原因: {}".format(ex))
