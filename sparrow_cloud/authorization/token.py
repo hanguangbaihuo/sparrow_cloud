d62eb49b6feb313d0320511297989b7fe5b97317
littletiger7 <little_tiger7@163.com>
2020-07-07 17:13:05 +0800

    change get token

--- sparrow_cloud/authorization/token.py
+++ sparrow_cloud/authorization/token.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 from django.core.cache import cache
 from sparrow_cloud.utils.get_hash_key import get_hash_key
@@ -20,6 +21,7 @@ def get_user_token(user_id):
     :return:
     """
     user_token_key = get_hash_key(key_type="user", user_id=user_id)
+    service_conf = get_settings_value("SERVICE_CONF")
     registry_app_conf = get_settings_value("REGISTRY_APP_CONF")
     enable_get_token_cache = registry_app_conf["ENABLE_TOKEN_CACHE"]
     if enable_get_token_cache:
@@ -28,9 +30,13 @@ def get_user_token(user_id):
             return cache_user_token["user_token"]
     try:
         data = {
+            "name": service_conf["NAME"],
+            "secret": service_conf["SECRET"],
             "uid": user_id
         }
-        user_token = rest_client.post(service_address=registry_app_conf["SERVICE_ADDRESS"], api_path=registry_app_conf["PATH"], timeout=0.5, data=data)
+        data_to_send = json.dumps(data).encode("utf-8")
+        user_token = rest_client.post(service_address=registry_app_conf["SERVICE_ADDRESS"],
+                                      api_path=registry_app_conf["PATH"], timeout=0.5, data=data_to_send)
         cache.set(user_token_key, {'user_token': user_token}, timeout=user_token["expires_in"]-120)
         return user_token
     except Exception as ex:
@@ -61,8 +67,9 @@ def get_app_token():
             "name": service_conf["NAME"],
             "secret": service_conf["SECRET"]
         }
+        data_to_send = json.dumps(data).encode("utf-8")
         app_token = rest_client.post(service_address=registry_app_conf["SERVICE_ADDRESS"],
-                                     api_path=registry_app_conf["PATH"], timeout=0.5, data=data)
+                                     api_path=registry_app_conf["PATH"], timeout=0.5, data=data_to_send)
         cache.set(app_token_key, {'app_token': app_token}, timeout=app_token["expires_in"]-120)
         return app_token
     except Exception as ex:
