4f83c8ec0b70900d54f312fad075cc31194bceba
waro163 <waro163@163.com>
2021-01-08 14:32:10 +0800

    add timeout for requests_client

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import sys
 import shutil
 from setuptools import find_packages, setup
 
-version = "v3.0.7"
+version = "v3.0.8"
 
 def read(f):
     return open(f, 'r', encoding='utf-8').read()
--- sparrow_cloud/restclient/requests_client.py
+++ sparrow_cloud/restclient/requests_client.py
@@ -13,7 +13,7 @@ from sparrow_cloud.utils.get_settings_value import get_service_name
 logger = logging.getLogger(__name__)
 
 
-def request(method, service_address, api_path, timeout, protocol="http", token=None, *args, **kwargs):
+def request(method, service_address, api_path, protocol="http", token=None, *args, **kwargs):
     service_name = get_service_name()
     request_url = build_url(protocol=protocol, address=service_address, api_path=api_path)
     headers = kwargs.pop('headers', {})
