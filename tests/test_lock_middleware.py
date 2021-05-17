964c036c0a396694eb1359b348cde474c61e3082
waro163 <waro163@163.com>
2021-05-17 11:34:49 +0800

    add comments for lock_middleware testcase

--- tests/test_lock_middleware.py
+++ tests/test_lock_middleware.py
@@ -30,11 +30,12 @@ class TestLockMiddleware(unittest.TestCase):
 
     @mock.patch('sparrow_cloud.restclient.rest_client.put', return_value={"code":0})
     def test_process_request_1(self,allow_mock):
+        # 测试header中携带前端锁的key，且锁服务返回正常
         self.assertEqual(self.middleware.process_request(MockRequest()),None)
 
     @mock.patch('sparrow_cloud.restclient.rest_client.put', return_value={"code":1})
     def test_process_request_2(self,deny_mock):
-        # import pdb; pdb.set_trace()
+        # 测试header中携带前端锁的key，且锁服务返回拒绝本次请求
         response = self.middleware.process_request(MockRequest())
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"code", response.content)
@@ -42,24 +43,29 @@ class TestLockMiddleware(unittest.TestCase):
 
     @mock.patch('sparrow_cloud.restclient.rest_client.put', return_value={"code":0})
     def test_process_request_3(self,empty_header):
+        # 测试header中携带前端锁的key为空情况，且锁服务返回正常
         self.assertEqual(self.middleware.process_request(MockEmptyRequest()),None)
 
     @mock.patch('sparrow_cloud.restclient.rest_client.put', return_value={"code":0})
     def test_process_request_4(self,no_header):
+        # 测试header中不携带前端锁的key，且锁服务返回正常
         self.assertEqual(self.middleware.process_request(MockNoRequest()),None)
 
     @mock.patch('sparrow_cloud.restclient.rest_client.delete', return_value={"code":0})
     def test_process_response_1(self, header):
+        # 测试header中携带前端锁的key，response处理返回正常
         response = self.middleware.process_response(MockRequest(), MockResponse())
         self.assertEqual(response.status_code, 200)
 
     @mock.patch('sparrow_cloud.restclient.rest_client.put', return_value={"code":0})
     def test_process_response_2(self, header):
+        # 测试header中携带前端锁的key，response处理返回异常
         response = self.middleware.process_response(MockRequest(), MockWrongResponse())
         self.assertEqual(response.status_code, MockWrongResponse().status_code)
     
     @mock.patch('sparrow_cloud.restclient.rest_client.delete', return_value={"code":0})
     def test_process_response_3(self, no_header):
+        # 测试header中不携带前端锁的key，response处理返回正常
         response = self.middleware.process_response(MockNoRequest(), MockResponse())
         self.assertEqual(response.status_code, 200)
 
