9ec8219c798d56e841a3854d9054dbdb546742b6
waro163 <waro163@163.com>
2021-06-11 17:55:25 +0800

    modify the get_user_token and get_app_token in authorization module

--- sparrow_cloud/authorization/token.py
+++ sparrow_cloud/authorization/token.py
@@ -8,7 +8,7 @@ from django.core.cache import cache
 from sparrow_cloud.utils.get_hash_key import get_hash_key
 from sparrow_cloud.utils.get_cm_value import get_cm_value
 from sparrow_cloud.utils.get_settings_value import get_settings_value
-from sparrow_cloud.restclient import rest_client
+from sparrow_cloud.restclient import rest_client, requests_client
 
 logger = logging.getLogger(__name__)
 
@@ -29,20 +29,25 @@ def get_user_token(user_id):
     try:
         cache_user_token = cache.get(user_token_key)
         if cache_user_token is not None:
-            return cache_user_token["user_token"]["token"]
+            return cache_user_token
+            # return cache_user_token["user_token"]["token"]
         else:
             data = {
                 "name": service_conf["NAME"],
                 "secret": service_conf["SECRET"],
                 "uid": user_id
             }
-            user_token = rest_client.post(service_address=sc_manage_svc, api_path=sc_manage_api, json=data)
-            cache.set(user_token_key, {'user_token': user_token}, timeout=user_token["expires_in"]-120)
-            logger.info("sparrowcloud get user token: {}".format(user_token["token"]))
-            return user_token["token"]
+            res = requests_client.post(service_address=sc_manage_svc, api_path=sc_manage_api, json=data)
+            if res.status_code<200 or res.status_code>=300:
+                raise Exception(f"请求app_manage服务出错，状态码:{res.status_code},数据:{res.text}")
+            cache.set(user_token_key, res.text, timeout=7200)
+            return res.text
     except Exception as ex:
-        raise Exception('sparrowcloud/get_user_token error, no token available in cache and app_manage_error, '
-                        'message:{}'.format(ex.__str__()))
+        logger.error(f"sparrowcloud/get_user_token error, no token available in cache and app_manage_error,\
+                        'message:{ex.__str__()}, return static token")
+        return json.dumps({"iss":"sparrow_cloud", "uid":user_id, "type": "user"})
+        # raise Exception('sparrowcloud/get_user_token error, no token available in cache and app_manage_error, '
+                        # 'message:{}'.format(ex.__str__()))
 
 
 def get_app_token():
@@ -59,18 +64,24 @@ def get_app_token():
     try:
         cache_app_token = cache.get(app_token_key)
         if cache_app_token:
-            return cache_app_token["app_token"]["token"]
+            return cache_app_token
+            # return cache_app_token["app_token"]["token"]
         else:
             data = {
                 "name": service_conf["NAME"],
                 "secret": service_conf["SECRET"]
             }
-            app_token = rest_client.post(service_address=sc_manage_svc, api_path=sc_manage_api, json=data)
-            cache.set(app_token_key, {'app_token': app_token}, timeout=app_token["expires_in"]-120)
-            logger.info("sparrowcloud get app token: {}".format(app_token["token"]))
-            return app_token["token"]
+            res = requests_client.post(service_address=sc_manage_svc, api_path=sc_manage_api, json=data)
+            if res.status_code<200 or res.status_code>=300:
+                raise Exception(f"请求app_manage服务出错，状态码:{res.status_code},数据:{res.text}")
+            cache.set(app_token_key, res.text, timeout=7200)
+            # logger.info("sparrowcloud get app token: {}".format(app_token["token"]))
+            return res.text
     except Exception as ex:
-        raise Exception('sparrowcloud/get_app_token error, no token available in cache and app_manage_error, '
-                        'message:{}'.format(ex.__str__()))
+        logger.error(f"sparrowcloud/get_app_token error, no token available in cache and app_manage_error,\
+                        'message:{ex.__str__()}, return static token")
+        return json.dumps({"iss":"sparrow_cloud", "uid":service_conf["NAME"], "type": "app"})
+        # raise Exception('sparrowcloud/get_app_token error, no token available in cache and app_manage_error, '
+        #                 'message:{}'.format(ex.__str__()))
 
 
--- tests/test_get_token_sdk.py
+++ tests/test_get_token_sdk.py
@@ -1,18 +1,39 @@
 import django
+import json
 import unittest
 from unittest import mock
 from django.core.cache import cache
 from sparrow_cloud.authorization.token import *
 from sparrow_cloud.utils.get_hash_key import get_hash_key
 
-
-MOCK_RESPONSE = {
-    "expires_in": 7200,
-    "token": "eyJhbGciOiJIUzI1.eyJ1aWQiOcCJ9.cayn"
+MOCK_USER_RESPONSE = {
+    "iss": "sparrow_app_manage",
+    "exp": 1622800021,
+    "iat": 1622792821,
+    "uid": "test_user_id_succ_abc123",
+    "type": "user"
+}
+MOCK_APP_RESPONSE = {
+    "iss": "sparrow_app_manage",
+    "exp": 1622800182,
+    "iat": 1622792982,
+    "uid": "sparrow_cloud",
+    "type": "app"
 }
 
+class UserGoodResp:
+    status_code = 200
+    text = json.dumps(MOCK_USER_RESPONSE)
 
-class TestGetAppToken(unittest.TestCase):
+class BadResp:
+    status_code = 400
+    text = '{"message":"bad request"}'
+
+class AppGoodResp:
+    status_code = 200
+    text = json.dumps(MOCK_APP_RESPONSE)
+
+class TestGetUserToken(unittest.TestCase):
 
     def setUp(self):
         os.environ["DJANGO_SETTINGS_MODULE"] = "tests.mock_settings"
@@ -20,32 +41,79 @@ class TestGetAppToken(unittest.TestCase):
         self.setup_settings(settings)
         django.setup()
 
-    @mock.patch('sparrow_cloud.restclient.rest_client.post', return_value=MOCK_RESPONSE)
-    def test_get_user_token(self, mock_post):
-        user_token = get_user_token(user_id="21424kvjbcdjslafds")
-        self.assertEqual(user_token, MOCK_RESPONSE["token"])
-    
-    def test_get_user_token_cache(self):
-        user_token_key = get_hash_key(key_type="user", user_id="21424kvjbcdjslafds")
-        cache.set(user_token_key, {'user_token': MOCK_RESPONSE}, timeout=MOCK_RESPONSE["expires_in"]-120)
-        token = get_user_token(user_id="21424kvjbcdjslafds")
-        cache.delete(user_token_key)
-        self.assertEqual(token, MOCK_RESPONSE["token"])
+    def setup_settings(self, settings):
+        settings.SERVICE_CONF = {
+            "NAME": "sparrow_cloud",
+            "SECRET": "124cdsalhdisahgisabjnjlk"
+        }
 
-    @mock.patch('sparrow_cloud.restclient.rest_client.post', return_value=MOCK_RESPONSE)
-    def test_get_app_token(self, mock_post):
-        app_token = get_app_token()
-        self.assertEqual(app_token, MOCK_RESPONSE["token"])
+    @mock.patch('sparrow_cloud.restclient.requests_client.post', return_value=UserGoodResp)
+    def test_get_user_token_succ(self, mock_post):
+        token = get_user_token(user_id="test_user_id_succ_abc123")
+        data = json.loads(token)
+        self.assertIn("uid", data)
+        self.assertEqual(data["uid"], MOCK_USER_RESPONSE["uid"])
+
+    @mock.patch('sparrow_cloud.restclient.requests_client.post', return_value=BadResp)
+    def test_get_user_token_fail(self, mock_post):
+        token = get_user_token(user_id="fail_user_id_abc123")
+        data = json.loads(token)
+        # data为返回的静态数据，没有iat和exp键
+        self.assertIn("uid", data)
+        self.assertNotIn("exp", data)
+        self.assertNotIn("iat", data)
     
-    def test_get_app_token_cache(self):
-        app_token_key = get_hash_key(key_type="app")
-        cache.set(app_token_key, {'app_token': MOCK_RESPONSE}, timeout=MOCK_RESPONSE["expires_in"]-120)
-        app_token = get_app_token()
-        cache.delete(app_token_key)
-        self.assertEqual(app_token, MOCK_RESPONSE["token"])
+    @mock.patch('sparrow_cloud.restclient.requests_client.post', return_value=UserGoodResp)
+    def test_get_user_token_cache(self, mock_post):
+        uid="test_cache_user_id"
+        token = get_user_token(user_id=uid)
+        data = json.loads(token)
+        self.assertIn("uid", data)
+        # 测试一次请求之后，检查缓存中是否已经缓存该token，并且缓存中与函数返回的数据相同
+        user_token_key = get_hash_key(key_type="user", user_id=uid)
+        cache_user_token = cache.get(user_token_key)
+        self.assertEqual(token, cache_user_token)
+
+
+class TestGetAppToken(unittest.TestCase):
+    def setUp(self):
+        os.environ["DJANGO_SETTINGS_MODULE"] = "tests.mock_settings"
+        from django.conf import settings
+        self.setup_settings(settings)
+        django.setup()
 
     def setup_settings(self, settings):
         settings.SERVICE_CONF = {
             "NAME": "sparrow_cloud",
             "SECRET": "124cdsalhdisahgisabjnjlk"
         }
+
+    @mock.patch('sparrow_cloud.restclient.requests_client.post', return_value=AppGoodResp)
+    def test_get_app_token_succ(self, mock_post):
+        app_token = get_app_token()
+        data = json.loads(app_token)
+        self.assertIn("uid", data)
+        self.assertEqual(data["uid"], MOCK_APP_RESPONSE["uid"])
+
+    @mock.patch('sparrow_cloud.restclient.requests_client.post', return_value=BadResp)
+    def test_get_app_token_fail(self, mock_post):
+        token = get_app_token()
+        data = json.loads(token)
+        # data为返回的静态数据，没有iat和exp键
+        self.assertIn("uid", data)
+        self.assertNotIn("exp", data)
+        self.assertNotIn("iat", data)
+    
+    @mock.patch('sparrow_cloud.restclient.requests_client.post', return_value=AppGoodResp)
+    def test_get_app_token_cache(self, mock_post):
+        app_token = get_app_token()
+        data = json.loads(app_token)
+        self.assertIn("uid", data)
+        # 测试一次请求之后，检查缓存中是否已经缓存该token，并且缓存中与函数返回的数据相同
+        app_token_key = get_hash_key(key_type="app")
+        cache_app_token = cache.get(app_token_key)
+        self.assertEqual(app_token, cache_app_token)
+    
+    def tearDown(self):
+        app_token_key = get_hash_key(key_type="app")
+        cache.delete(app_token_key)
